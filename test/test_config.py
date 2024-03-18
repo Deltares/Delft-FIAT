9ab20af6c3c762edf1fb8b4a20f460ed6941c1bf
Brendan <brencodeert@outlook.com>
2024-03-18 16:40:05 +0100

    Update settings testr

--- test/test_config.py
+++ test/test_config.py
@@ -2,5 +2,18 @@ from fiat.cfg import ConfigReader
 
 
 def test_settings(settings_files):
-    ConfigReader(settings_files["geom_event"])
-    pass
+    cfg = ConfigReader(settings_files["geom_risk"])
+
+    # Assert path to itself
+    assert cfg.filepath.name == "geom_risk.toml"
+
+    # Assert a global setting
+    assert cfg.get("global.keep_temp_files")
+
+    # Assert generated kwargs functionality
+    haz_kw = cfg.generate_kwargs("hazard.settings")
+    assert "var_as_band" in haz_kw
+
+    # Assert updating leads to extra kwargs
+    cfg.set("global.is_here", True)
+    assert "global.is_here" in cfg._extra_args
