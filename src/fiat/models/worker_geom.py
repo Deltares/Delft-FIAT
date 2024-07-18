f5772cfc791b9a86f3df2d62891b7ada35319b87
Brendan <brencodeert@outlook.com>
2024-07-18 16:46:36 +0200

    Some progress

--- src/fiat/models/worker_geom.py
+++ src/fiat/models/worker_geom.py
@@ -1,12 +1,37 @@
 """Worker function for the geometry model (no csv)."""
 
+from math import nan
+from multiprocessing.synchronize import Lock
+
+from fiat.gis import geom, overlay
 from fiat.io import GridSource, Table
 
 
 def worker(
+    cfg,
     haz: GridSource,
-    exp: dict,
+    idx: int,
+    exp_geom: dict,
     vul: Table,
+    chunk: tuple | list,
+    lock: Lock,
 ):
     """_summary_."""
+    band = haz[idx]
+
+    for _, gm in exp_geom.items():
+        # Check if there actually is data for this chunk
+        if chunk[0] > gm._count:
+            continue
+
+        # Loop over all the geometries in a reduced manner
+        for ft in gm.reduced_iter(*chunk):
+            # How to get the hazard data
+            if ft.GetField("extract_method") == "area":
+                res = overlay.clip(band, haz.get_srs(), haz.get_geotransform(), ft)
+            else:
+                res = overlay.pin(band, haz.get_geotransform(), geom.point_in_geom(ft))
+
+            res[res == band.nodata] = nan
+            pass
     pass
