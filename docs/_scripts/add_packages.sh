1cebf75beeadd4c8782cdd03645fb54ab057f308
Brendan <brencodeert@outlook.com>
2025-01-23 14:17:45 +0100

    Quarto fix

--- docs/_scripts/add_packages.sh
+++ docs/_scripts/add_packages.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/expect -f
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-PROJECTPATH=$(dirname $(dirname "$SCRIPTPATH"))
-
-# Shell hook
-pixi shell-hook --manifest-path $PROJECTPATH/pyproject.toml -s bash -e build-linux > $PROJECTPATH/pixi-hook.rc
-source $PROJECTPATH/pixi-hook.rc
 
 # Define your list of packages
 set packages {machow/quartodoc pandoc-ext/section-bibliographies quarto-ext/fontawesome quarto-ext/include-code-files}
