856a4bb235ebcf7d7cb6f1661eca409f33e2055f
dalmijn <brencodeert@outlook.com>
2025-05-23 16:35:50 +0200

    cli: better parsing of entry setting

--- src/fiat/cli/action.py
+++ src/fiat/cli/action.py
@@ -1,6 +1,28 @@
 """Custom actions for cli."""
 
 import argparse
+import re
+
+from fiat.util import _dtypes_reversed, deter_type
+
+
+def parse_cli_value(value: str) -> object:
+    """Parse the value to a python type."""
+    value = value.strip()
+    m = re.findall(r"^\[(.*)\]$", value)  # Pattern for checking list
+    # If not a list, then it's a single value
+    if len(m) == 0:
+        t = deter_type(value.encode(), 0)
+        value = _dtypes_reversed[t](value)
+        return value
+
+    # If it's a list
+    item = m[0]
+    item = item.replace(" ", "")  # Strip it from spaces
+    # Detemine the type of the list and use that to set the types of the elements
+    t = deter_type(item.replace(",", "\n").encode(), item.count(","))
+    value = [_dtypes_reversed[t](elem) for elem in item.split(",")]
+    return value
 
 
 class KeyValueAction(argparse.Action):
@@ -16,7 +38,7 @@ class KeyValueAction(argparse.Action):
         try:
             key_value_dict = getattr(namespace, self.dest)
             key, value = values.split("=", 1)
-            key_value_dict[key] = value
+            key_value_dict[key] = parse_cli_value(value)
             setattr(namespace, self.dest, key_value_dict)
         except BaseException:
             parser.error(f"-d, key/ value pair in the wrong format: -> '{values}'. \
