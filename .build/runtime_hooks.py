295e6cb1bf7101aff2db71451af1386cd0a84674
dalmijn <92092029+dalmijn@users.noreply.github.com>
2025-11-25 15:30:36 +0100

    Fix build hook osgeo (#196)

--- .build/runtime_hooks.py
+++ .build/runtime_hooks.py
@@ -7,11 +7,12 @@ from pathlib import Path
 # Path to executable
 cwd = Path(sys.executable).parent
 
-# Paths to libaries
-os.environ["GDAL_DRIVER_PATH"] = Path(cwd, "bin", "gdalplugins").as_posix()
+# Paths to libaries/ data
+os.environ["GDAL_DATA"] = str(Path(cwd, "bin", "share", "gdal"))
+os.environ["GDAL_DRIVER_PATH"] = str(Path(cwd, "bin", "gdalplugins"))
 # Newer versions of GDAL and PROJ
-os.environ["PROJ_DATA"] = str(Path(cwd, "bin", "share"))
+os.environ["PROJ_DATA"] = str(Path(cwd, "bin", "share", "proj"))
 # Older versions of GDAL and PROJ
-os.environ["PROJ_LIB"] = str(Path(cwd, "bin", "share"))
+os.environ["PROJ_LIB"] = str(Path(cwd, "bin", "share", "proj"))
 # Append to path
 sys.path.append(str(Path(cwd, "bin", "share")))
